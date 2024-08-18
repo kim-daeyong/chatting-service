7e716a0a7239af91a8f43083aa0e4ca33e1e671c
kim-daeyong <kdy2353@gmail.com>
2024-08-19 01:13:46 +0900

    chore(data.sql) add data.sql for user

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+-- User
+INSERT INTO users (id, user_id, email)
+VALUES
+    (1, 'user1', 'user1@example.com'),
+    (2, 'user2', 'user2@example.com'),
+    (3, 'user3', 'user3@example.com'),
+    (4, 'user4', 'user4@example.com'),
+    (5, 'user5', 'user5@example.com');
